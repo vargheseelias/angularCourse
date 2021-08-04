2d2d91a9172deccfae6fe7e982fbd29cac3a1f4b
vargheseelias <vargheseelias10@gmail.com>
2021-08-04 11:30:00 +0530

    angulr crse 2

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,19 @@
+import { ElementRef } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  // @HostBinding('class.open') isOpen = false;
+
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  @HostBinding('class.open') isOpen = false;
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
+}
