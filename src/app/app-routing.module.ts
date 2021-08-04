5753b39d2bf9a811b20195c434d49bf53de0cc01
vargheseelias <vargheseelias10@gmail.com>
2021-08-04 13:51:08 +0530

    angulr crse sec19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -12,8 +13,8 @@ const appRoutes: Routes = [
   { path: 'recipes', component: RecipesComponent, children: [
     { path: '', component: RecipeStartComponent},
     { path: 'new', component: RecipeEditComponent},
-    { path: ':id', component: RecipeDetailComponent},
-    { path: ':id/edit', component: RecipeEditComponent}
+    { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+    { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}
   ]},
   { path: 'shoping-list', component: ShoppingListComponent}
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,4 @@
+import { DataStorageService } from './../shared/data-storage.service';
 import { Component, } from '@angular/core';
 
 @Component({
@@ -5,7 +6,14 @@ import { Component, } from '@angular/core';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
 
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 
 }
